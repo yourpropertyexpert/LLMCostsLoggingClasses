6b1b83a627478b7bec22f6d66d2687b616680575
Mark Harrison <yourpropertyexpert@gmail.com>
2025-02-07 12:49:23 +0000

    First cut

---
+++ PHP/LLMCostLogger.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * LLMCostLogger class
+ * @copyright Mark Harrison 2025
+ * @package LLMCosts
+ */
+
+namespace LLMCosts;
+
+
+class LLMCostLogger
+{
+    private \GuzzleHttp\Client $client;
+    private const string URI ="https://llmcosts.fyi/api/logcall";
+
+    // Constructor property promotion
+    public function __construct(private string $key = "")
+    {
+        if ($key = "") {
+            throw new \Exception("No Key Provided");
+        }
+        $this->client = new \GuzzleHttp\Client();
+    }
+
+    public function logCall(string $provider, array $body)
+    {
+        if ($provider == "OpenAI") {
+            // Attempt to strip response content leaving only the metadata
+            $body["choices"] = [];
+        }
+        // Make a Guzzle HTTP call
+        try {
+            $response = $this->client->post(self::URI, [
+                'form_params' => [
+                    'apikey' => $this->key,
+                    'provider' => $provider,
+                    'body' => json_encode($body),
+                ],
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            throw new \Exception("Failed to log call: " . $e->getMessage());
+        }
+    }
+}
